fdb5c9717c8b65c7cbd8443555a88fbd55d7499d
Blade12629 <blade1234@web.de>
2021-04-17 04:58:52 +0200

    Forgot to comment out and to remove old script file

--- Skyfly/UODisc/Chats/Custom/KnivesChat/GameChatSync.cs
+++ Skyfly/UODisc/Chats/Custom/KnivesChat/GameChatSync.cs
@@ -1,64 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Knives.Chat3;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Knives.Chat3;
 
-namespace Server.Custom.Skyfly.UODisc.Chats.Custom.KnivesChat
-{
-	public class GameChatSync : BaseChatSync
-	{
+//namespace Server.Custom.Skyfly.UODisc.Chats.Custom.KnivesChat
+//{
+//	public class GameChatSync : BaseChatSync
+//	{
 
-		public Channel Channel { get; private set; }
-		public string ChannelName { get; private set; }
-		public Mobile FakeMobile { get; private set; }
+//		public Channel Channel { get; private set; }
+//		public string ChannelName { get; private set; }
+//		public Mobile FakeMobile { get; private set; }
 
-		public static readonly string FakeMobileTitle = "GameChatSyncFakeMobile";
+//		public static readonly string FakeMobileTitle = "GameChatSyncFakeMobile";
 
-		public GameChatSync(string channel)
-		{
-			ChannelName = channel;
-		}
+//		public GameChatSync(string channel)
+//		{
+//			ChannelName = channel;
+//		}
 
-		public override void Start()
-		{
-			if (IsRunning)
-				return;
+//		public override void Start()
+//		{
+//			if (IsRunning)
+//				return;
 
-			Channel = Channel.GetByName(ChannelName);
+//			Channel = Channel.GetByName(ChannelName);
 
-			if (Channel == null)
-				return;
+//			if (Channel == null)
+//				return;
 
-			FakeMobile = World.Mobiles.Values.FirstOrDefault(m => m.Map == Map.Internal && m.Title != null && m.Title.Equals(FakeMobileTitle));
+//			FakeMobile = World.Mobiles.Values.FirstOrDefault(m => m.Map == Map.Internal && m.Title != null && m.Title.Equals(FakeMobileTitle));
 
-			if (FakeMobile == null)
-				FakeMobile = GetFakeMobile();
+//			if (FakeMobile == null)
+//				FakeMobile = GetFakeMobile();
 
-			base.Start();
-		}
+//			base.Start();
+//		}
 
-		Mobile GetFakeMobile()
-		{
-			if (FakeMobile != null && !FakeMobile.Deleted)
-				return FakeMobile;
+//		Mobile GetFakeMobile()
+//		{
+//			if (FakeMobile != null && !FakeMobile.Deleted)
+//				return FakeMobile;
 
-			Mobile m = new Mobile();
-			m.Title = FakeMobileTitle;
-			m.MoveToWorld(Point3D.Zero, Map.Internal);
+//			Mobile m = new Mobile();
+//			m.Title = FakeMobileTitle;
+//			m.MoveToWorld(Point3D.Zero, Map.Internal);
 
-			return m;
-		}
+//			return m;
+//		}
 
-		protected virtual void SendMessage(Mobile from, string msg)
-		{
-			Channel.OnChat(from, msg, false);
-		}
+//		protected virtual void SendMessage(Mobile from, string msg)
+//		{
+//			Channel.OnChat(from, msg, false);
+//		}
 
-		protected override void SendMessage(string msg)
-		{
-			SendMessage(FakeMobile, msg);
-		}
-	}
-}
+//		protected override void SendMessage(string msg)
+//		{
+//			SendMessage(FakeMobile, msg);
+//		}
+//	}
+//}
